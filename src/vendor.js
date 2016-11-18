5a4df34460acfcf9306ca768fc61b0336c48aa0a
guangyan.yao <guangyan.yao@qunar.com>
2016-11-18 14:58:17 +0800

    mailstone

---
+++ src/vendor.js
@@ -0,0 +1,7 @@
+// vendor
+import 'jquery'
+import 'vue'
+import 'vue-resource'
+import 'vue-router'
+import 'vuex'
+import 'bootstrap-loader'
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
-var package = require('./package.json')
-var vendor = Object.keys(package.dependencies)
 
 module.exports = {
     entry: {
         app: './src/app.js',
-        vendor: vendor
+        vendor: './src/vendor.js'
     },
     output: {
         path: path.resolve(__dirname, './dist'),
@@ -17,17 +17,70 @@ module.exports = {
         loaders: [
             {
                 test: /\.vue$/,
-                loader: 'vue'
+                loader: ['vue-loader']
             },
             {
                 test: /\.js$/,
-                loader: 'babel',
+                loader: ['babel-loader'],
                 exclude: /node_modules/
-            }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style',
+                    loader: 'css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]' +
+                    '!postcss',
+                }),
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallbackLoader: 'style',
+                    loader: 'css?modules&importLoaders=2&localIdentName=[name]__[local]__[hash:base64:5]' +
+                    '!postcss' +
+                    '!sass',
+                }),
+            },
+
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                // Limiting the size of the woff fonts breaks font-awesome ONLY for the extract text plugin
+                // loader: "url?limit=10000"
+                loader: 'url',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                loader: 'file',
+            },
+            {test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery'}
         ]
     },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js'),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor']
+        }),
+        new ExtractTextPlugin({ filename: 'app.css', allChunks: true }),
+        new webpack.LoaderOptionsPlugin({
+            postcss: [autoprefixer],
+        }),
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery',
+            jquery: 'jquery',
+            "window.moment": "moment",
+            'Tether': 'tether',
+            'window.Tether': 'tether',
+            Tooltip: "exports?Tooltip!bootstrap/js/dist/tooltip",
+            Alert: "exports?Alert!bootstrap/js/dist/alert",
+            Button: "exports?Button!bootstrap/js/dist/button",
+            Carousel: "exports?Carousel!bootstrap/js/dist/carousel",
+            Collapse: "exports?Collapse!bootstrap/js/dist/collapse",
+            Dropdown: "exports?Dropdown!bootstrap/js/dist/dropdown",
+            Modal: "exports?Modal!bootstrap/js/dist/modal",
+            Popover: "exports?Popover!bootstrap/js/dist/popover",
+            Scrollspy: "exports?Scrollspy!bootstrap/js/dist/scrollspy",
+            Tab: "exports?Tab!bootstrap/js/dist/tab",
+            Util: "exports?Util!bootstrap/js/dist/util"
+        })
     ]
 }
